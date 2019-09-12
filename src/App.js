9eb383d986ed9972c3e9eed76a5354fa6d697361
Joe <josephc.lambert@gmail.com>
2019-09-11 17:24:47 -0700

    adds moment and stubs sample dates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import MediaContainer from './components/Media/MediaContainer';
+import axios from 'axios';
+import moment from 'moment';
+
+const url = `https://api.nasa.gov/planetary/apod?api_key=QYV95kw1NSpdnVcv3dE3zn4UEOlfuhbT69Z2ox3s&date=`;
 
 function App() {
+  const [media, setMedia] = useState({});
+  const [date, setDate] = useState(moment().format('YYYY-MM-DD'));
+
+  const format = `YYYY-MM-DD`;
+  console.log(
+    'yesterday',
+    moment()
+      .subtract(1, 'days')
+      .format(format)
+  );
+  console.log('today', moment().format(format));
+  console.log(
+    'tomorrow',
+    moment()
+      .add(1, 'days')
+      .format(format)
+  );
+
+  async function getData(urls) {
+    const [previous, current, next] = await Promise.all(urls.map(url => axios.get(url)));
+  }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      // const data = await getData(urls);
+    };
+    axios
+      .get(url)
+      .then(response => {
+        console.log(response);
+        setMedia(prevState => {
+          return { ...prevState, currentDay: response.data };
+        });
+      })
+      .catch(error => {
+        console.log(`there was an error fetching ${url}`);
+        console.error(error);
+      });
+  }, []);
+
   return (
     <>
-      <MediaContainer />
+      <MediaContainer media={media} />
     </>
   );
 }
--- src/components/Media/MediaContainer.js
+++ src/components/Media/MediaContainer.js
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
 
 // example call with for video https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2014-03-12
 // example call with for photo https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY
@@ -7,6 +6,8 @@ import axios from 'axios';
 // set state with response
 // conditionally render Photo or Video depending on media_type
 
-export default function MediaContainer() {
+const MediaContainer = ({ media }) => {
   return <p>Media Container</p>;
-}
+};
+
+export default MediaContainer;

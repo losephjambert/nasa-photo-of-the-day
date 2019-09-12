b4dd499290951a066f27fe63ca919d8d2d205189
Joe <josephc.lambert@gmail.com>
2019-09-12 13:56:46 -0700

    fetches media. stores media in state. changing days refetches media and stores in media state.

--- src/App.js
+++ src/App.js
@@ -3,104 +3,42 @@ import MediaContainer from './components/Media/MediaContainer';
 import axios from 'axios';
 import moment from 'moment';
 
-const startdate = new moment();
+const _startdate = new moment();
 const url = `https://api.nasa.gov/planetary/apod?api_key=QYV95kw1NSpdnVcv3dE3zn4UEOlfuhbT69Z2ox3s&date=`;
 
 function App() {
   const format = `YYYY-MM-DD`;
-
+  const [startdate] = useState(_startdate);
+  const [isLoading, setIsLoading] = useState(true);
   const [media, setMedia] = useState({});
-  const [currentDate, setCurrentDate] = useState(startdate);
-  // const [dateWindow, setDateWindow] = useState([null, null, null]);
-
-  async function getData(urls) {
-    let [previous, current, next] = await Promise.all(urls.map(url => axios.get(url)));
-  }
+  const [currentDate, setCurrentDate] = useState(_startdate);
 
   useEffect(() => {
-    // const fetchData = async () => {
-    //   const data = await getData([currentDate]);
-    // };
-    // axios
-    //   .get(url + currentDate.format(`YYYY-MM-DD`))
-    //   .then(response => {
-    //     setMedia(response.data);
-    //   })
-    //   .catch(error => {
-    //     console.log(`there was an error fetching ${url}`);
-    //     console.error(error);
-    //   });
-
-    // handleDateWindow(currentDate);
-    const currentDateMinus1 = currentDate.clone().subtract(1, 'days');
-    const currentDateClone = currentDate.clone();
-    const currentDatePlus1 = currentDate.clone().add(1, 'days');
-    const dateWindow = [currentDateMinus1, currentDateClone, currentDatePlus1].map(date =>
-      axios.get(url + date.format(`YYYY-MM-DD`))
-    );
-
-    const dateWindowResolved = dateWindow.map(window => window.catch(err => ({ err })));
-    const checkIfRequestFailed = then => {
-      return function(responses) {
-        const failed = responses.some(response => response.error);
-
-        if (failed) throw responses;
-
-        return then(responses);
-      };
-    };
     axios
-      .all(dateWindowResolved)
-      .then(
-        checkIfRequestFailed((...dateWindow) => {
-          console.log('success', ...dateWindow);
-        })
-      )
-      .catch(err => {
-        console.log('there was an error fetching that url');
-        console.error(err);
+      .get(url + currentDate.format(`YYYY-MM-DD`))
+      .then(response => {
+        setMedia(response.data);
+        setIsLoading(isLoading => !isLoading);
+      })
+      .catch(error => {
+        console.log(`there was an error fetching ${url}`);
+        console.error(error);
       });
-    // axios.all(dateWindow.map(url => axios.get(url))).then(
-    //   axios.spread((previous, current, next) => {
-    //     console.log(previous, current, next);
-    //   })
-    // );
-    // dateWindow.forEach(url => {
-    //   axios
-    //     .get(url)
-    //     .then(response => {
-    //       console.log(response);
-    //     })
-    //     .catch(error => {
-    //       console.log(error);
-    //     });
-    // });
-
-    // setDateWindow([currentDateMinus1, currentDateClone, currentDatePlus1]);
   }, [currentDate]);
 
   const handleDateChange = day => {
+    setIsLoading(isLoading => !isLoading);
     const newCurrentDate = currentDate.clone().subtract(day, 'days');
     setCurrentDate(newCurrentDate);
   };
 
-  // const handleDateWindow = currentDate => {
-  //   const currentDateMinus1 = currentDate.clone().subtract(1, 'days');
-  //   const currentDatePlus1 = currentDate.clone().add(1, 'days');
-  //   const currentDateClone = currentDate.clone();
-  //   setDateWindow([currentDateMinus1, currentDateClone, currentDatePlus1]);
-  // };
-
   return (
     <>
-      <MediaContainer media={''} />
+      <MediaContainer media={media} isLoading={isLoading} />
       <p>current date: {currentDate.format(format)}</p>
-      {/* {dateWindow.map((date, i) => (
-        <span key={i}>{date && date.format(format)} </span>
-      ))} */}
       <div>
         <button onClick={() => handleDateChange(1)}>Previous Day</button>
-        <button onClick={() => handleDateChange(-1)} disabled={currentDate.format(format) === moment().format(format)}>
+        <button onClick={() => handleDateChange(-1)} disabled={currentDate.format(format) === startdate.format(format)}>
           Next Day
         </button>
       </div>
--- src/components/Media/MediaContainer.js
+++ src/components/Media/MediaContainer.js
@@ -6,7 +6,9 @@ import React from 'react';
 // set state with response
 // conditionally render Photo or Video depending on media_type
 
-const MediaContainer = ({ media }) => {
+const MediaContainer = ({ media, isLoading }) => {
+  if (isLoading) return <p>LOADING IMAGE</p>;
+
   if (media.media_type === 'video') return <iframe src={media.url} frameBorder='0' title='video' />;
   else return <img style={{ width: '600px' }} src={media.url} alt='img' />;
 };

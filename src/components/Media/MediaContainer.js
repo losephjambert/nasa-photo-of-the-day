05e812bcf26bed3249357fc56747aff9aeba55cc
Joe <josephc.lambert@gmail.com>
2019-09-12 15:10:43 -0700

    removes comments from MediaContainer. changes laoding message

--- src/components/Media/MediaContainer.js
+++ src/components/Media/MediaContainer.js
@@ -1,13 +1,7 @@
 import React from 'react';
 
-// example call with for video https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2014-03-12
-// example call with for photo https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY
-// make api call
-// set state with response
-// conditionally render Photo or Video depending on media_type
-
 const MediaContainer = ({ media, isLoading }) => {
-  if (isLoading) return <p>LOADING IMAGE</p>;
+  if (isLoading) return <p>LOADING MEDIA</p>;
 
   if (media.media_type === 'video') return <iframe src={media.url} frameBorder='0' title='video' />;
   else return <img style={{ width: '600px' }} src={media.url} alt='img' />;
